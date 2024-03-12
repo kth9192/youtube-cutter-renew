f6b5eb0ffc984b1bfc4b95a572faa6178a122e8c
kth9193 <kth9193@gmail.com>
2024-03-12 17:52:12 +0900

    빌드수정

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,5 @@
-import client from '@/libs/server/client';
-import { generateRandomNumber } from '@/shared/utils';
-import NextAuth, { AuthOptions } from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
+import { authOptions } from '@/libs/server/auth';
+import NextAuth from 'next-auth';
 
 declare module 'next-auth' {
   interface User {
@@ -69,60 +67,6 @@ declare module 'next-auth' {
 //     },
 //   },
 // });
-
-export const authOptions: AuthOptions = {
-  providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        username: {
-          label: 'Username',
-          type: 'text',
-          placeholder: '이메일 입력',
-        },
-      },
-
-      async authorize(credentials, req) {
-        try {
-          if (typeof credentials !== 'undefined') {
-            const findUser = await client.user.findFirst({
-              where: {
-                email: credentials?.username,
-              },
-            });
-
-            if (findUser) {
-              console.log('user exist', findUser);
-
-              return findUser;
-            } else {
-              console.log('user not exist');
-
-              const newUser = await client.user.create({
-                data: {
-                  userId: generateRandomNumber(21),
-                  name: `testuser${generateRandomNumber(4)}`,
-                  email: `${generateRandomNumber(8)}@test.com`,
-                },
-              });
-
-              console.log('newuser', newUser);
-
-              return newUser;
-            }
-          } else {
-            return null;
-          }
-        } catch (error) {
-          throw error;
-        }
-      },
-    }),
-  ],
-  pages: {
-    signIn: '/auth/signin',
-  },
-};
 
 const handler = NextAuth(authOptions);
 
---
+++ libs/server/auth.ts
@@ -0,0 +1,58 @@
+import client from '@/libs/server/client';
+import { generateRandomNumber } from '@/shared/utils';
+import { AuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+export const authOptions: AuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: {
+          label: 'Username',
+          type: 'text',
+          placeholder: '이메일 입력',
+        },
+      },
+
+      async authorize(credentials, req) {
+        try {
+          if (typeof credentials !== 'undefined') {
+            const findUser = await client.user.findFirst({
+              where: {
+                email: credentials?.username,
+              },
+            });
+
+            if (findUser) {
+              console.log('user exist', findUser);
+
+              return findUser;
+            } else {
+              console.log('user not exist');
+
+              const newUser = await client.user.create({
+                data: {
+                  userId: generateRandomNumber(21),
+                  name: `testuser${generateRandomNumber(4)}`,
+                  email: `${generateRandomNumber(8)}@test.com`,
+                },
+              });
+
+              console.log('newuser', newUser);
+
+              return newUser;
+            }
+          } else {
+            return null;
+          }
+        } catch (error) {
+          throw error;
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/auth/signin',
+  },
+};
